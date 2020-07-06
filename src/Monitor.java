d6a3a11d790cdb0231a73694c0491d2b032219ef
Akamai98 <franbonino82@gmail.com>
2020-07-06 11:36:25 -0300

    Format correction

--- src/Monitor.java
+++ src/Monitor.java
@@ -16,6 +16,9 @@ public class Monitor {
     private PetriNet pNet;
 
     //Constructor
+    /**
+     * @param pNet This is the Petri net that will be controlled by the monitor.
+     */
     public Monitor(PetriNet pNet) {
         this.pNet = pNet;
         //TODO
